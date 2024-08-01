a9a7b9ee3aa33e36dc5e6f7c84314dd86abd5802
Dean Martin <dean.martin@derivco.co.za>
2024-08-01 13:18:23 +0200

    fix

--- src/components/PurchaseList.tsx
+++ src/components/PurchaseList.tsx
@@ -3,7 +3,8 @@ import { PurchaseItemCard } from "./PurchaseItemCard"
 export function PurchaseList(props: {
     purchaseItems: Array<{
         title: string,
-        subtitle: string
+        subtitle: string,
+        amount: number
     }>
 }){
     return (
@@ -11,6 +12,7 @@ export function PurchaseList(props: {
             {props.purchaseItems.map(i =>
                 <PurchaseItemCard
                     key={i.title}
+                    amount={i.amount}
                     title={i.title}
                     subtitle={i.subtitle}
                     backgroundImageUrl="purchase_item.png"></PurchaseItemCard>
