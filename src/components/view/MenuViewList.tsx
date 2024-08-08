2b838b7eccf401fd813ec6dd24e36e46227fc0fb
Dean Martin <dean.martin@derivco.co.za>
2024-08-08 12:20:43 +0200

    Adding of badges for entries etc.

--- src/components/view/MenuViewList.tsx
+++ src/components/view/MenuViewList.tsx
@@ -7,6 +7,10 @@ import { PayoutsList } from "./PayoutsList";
 import { useContext } from "react";
 import { UserContext } from "../../App";
 import { MainViews } from "../../enums/MainViews";
+import { Badge } from "react-bootstrap";
+import { ITransaction } from "../../interfaces/ITransaction";
+import { TransactionType } from "../../enums/TransactionTypes";
+import { GetTransactionsSinceFromTime } from "../../utilities/Transactions";
 
 const StyledContainer = styled.div`
     flex: 1;
@@ -22,18 +26,39 @@ const BalanceContainer = styled.div`
 
 export function MenuViewList(){
     const [state, dispatch] = useContext(UserContext);
+    const incomingTransactions: Array<ITransaction> = state
+        .transactionsQuery
+        .data
+        .filter(t => t.type == TransactionType.IncomingToMerchant);
+    const outgoingTransactions: Array<ITransaction> = state
+        .transactionsQuery
+        .data
+        .filter(t => t.type == TransactionType.OutgoingToPlayer);
+    const transactionsForCurrentPeriod: Array<ITransaction> = GetTransactionsSinceFromTime(state.periodStartDate, incomingTransactions);
 
     return (
         <StyledContainer>
             <BalanceContainer>Your Current Bets: {state.userBalance.toLocaleString()} TON</BalanceContainer>
             <Tabs defaultActiveKey={state.defaultMainView} className="mb-3" fill>
-                <Tab eventKey={MainViews.Bets} title="Bets" className="flex">
+                <Tab eventKey={MainViews.Bets} title={
+                    <>
+                    Bets <Badge bg="secondary">{state.userBalance}</Badge>
+                    </>
+                } className="flex" >
                     <PurchaseList></PurchaseList>
                 </Tab>
-                <Tab eventKey={MainViews.Entries} title="Entries" className="flex">
+                <Tab eventKey={MainViews.Entries} title={
+                    <>
+                    Entries <Badge bg="secondary">{transactionsForCurrentPeriod.length}</Badge>
+                    </>
+                } className="flex">
                     <EntriesList></EntriesList>
                 </Tab>
-                <Tab eventKey={MainViews.Payouts} title="Payouts" className="flex">
+                <Tab eventKey={MainViews.Payouts} title={
+                    <>
+                    Payouts <Badge bg="secondary">{outgoingTransactions.length}</Badge>
+                    </>
+                } className="flex">
                     <PayoutsList></PayoutsList>
                 </Tab>
             </Tabs>
